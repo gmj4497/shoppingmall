bb271b61b1ee494fb616c7f2c394c78360ee2b69
gmj4497 <58320566+gmj4497@users.noreply.github.com>
2019-12-24 12:28:14 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,47 @@
+// data.js
+
+/* 제품 정보 데이터 */
+var product_data =[
+    {
+    // 속성 값
+    img_url: "images/man1.jpg",
+    title: "PENFIELD",
+    name: "로리엣 X 펜필드 더블 헤비울 퍼 코트_FK4WJ77M",
+    origin_price: "1,099,000",
+    sale_price: "879,200"
+    },
+    {
+    // 속성 값
+    img_url: "images/man2.jpg",
+    title: "PENFIELD",
+    name: "로리엣 X 펜필드 헤리티지 발마칸 코트_FK4WJ75M",
+    origin_price: "899,000",
+    sale_price: "719,200"
+    },
+    {
+    // 속성 값
+    img_url: "images/man3.jpg",
+    title: "PENFIELD",
+    name: "로리엣 X 펜필드 트위드 4 포켓 자켓_FK4WJ78M",
+    origin_price: "699,000",
+    sale_price: "559,200"
+    },
+    {
+    // 속성 값
+    img_url: "images/man4.jpg",
+    title: "PENFIELD",
+    name: "로리엣 X 펜필드 구스 숏다운 점퍼_FK4WD77M",
+    origin_price: "368,000",
+    sale_price: "214,800"
+    },
+    {
+    // 속성 값
+    img_url: "images/man5.jpg",
+    title: "PENFIELD",
+    name: "로리엣 X 펜필드 구스 다운 퍼 점퍼_FK4WD75M",
+    origin_price: "1,399,000",
+    sale_price: "1,119,200"
+    },
+]; 
+
+
---
+++ js/main.js
@@ -0,0 +1,63 @@
+// main.js
+$(function(){
+
+
+  // 상품리스트 추가 
+  var item_list = $('.item_list');  //상품 목록 위치
+  var list = '';      // 삽입될 html , date
+  var list_num = 5;   // 상품목록 갯수
+  var kwd = '원'; 
+  var origin_price = ''; 
+   
+   
+  for(var i = 0; i < list_num; i++) { 
+    list += '<li>'; 
+    list += '<a href="#">'; 
+    list += '<img src="' + product_data[i].img_url + '" >'; 
+    list += '<span class="title">' + product_data[i].title + '</span>'; 
+    list += '<span class="name">' + product_data[i].name + '</span>' 
+ 
+   
+  if(product_data[i].origin_price == "") { 
+    kwd = "&nbsp;"; 
+    origin_price = ""; 
+  } else { origin_price = product_data[i].origin_price;} 
+
+   
+    list += '<del class="origin_price">' + origin_price + kwd + '</del>'; 
+    list += '<ins class="sale_price"><strong>' + product_data[i].sale_price + '원</strong></ins>'; 
+   } 
+  item_list.append(list); 
+
+
+
+
+
+   // 카테고리 토글 버튼
+
+  $('#category_btn').click(function(){
+    $('#category').slideToggle();
+  });
+
+  // 다른데 누르면 자동닫힘
+  $('header,section').click(function(){
+    $('#category').slideUp();
+  });
+
+
+
+
+
+  // BX 슬라이더
+  $('.bx-slider').bxSlider({
+    // 여기에 옵션 설정
+    mode: 'horizontal',     // 효과설정
+    auto: true,             // 자동재생
+    easing: "ease-in-out",  // 가속도(timing-function)
+    pause: 4000,            // 정지시간(대기)
+    controls: true,        // prev, next 표시/비표시
+    pager: true,           // 하단 인디케이터 표시/비표시
+    autoHover: true        // 마우스올라가면 재생정지
+  });
+
+});
